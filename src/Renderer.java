4c9d4f7a506ca011ff8a4beff6ef655582b42947
matthewfarrow <mattrfarrow@gmail.com>
2020-04-13 20:33:23 +0100

    fix imports

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,12 +1,10 @@
 import javafx.animation.AnimationTimer;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
